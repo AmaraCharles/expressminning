95e152e644166c72ae5bd7c95d45657deb7bfa9e
harold-ereama <ogemdiereama@gmail.com>
2023-02-24 11:07:43 +0100

    first commit

---
+++ Dashboard/forgot.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+<!-- Mirrored from expressmining.com/Dashboard/forgot.php by HTTrack Website Copier/3.x [XR&CO'2014], Sun, 19 Feb 2023 15:14:18 GMT -->
+<!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=0.8, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="../favicon.png" type="image/x-icon">
+    <title> Expressmining | Join </title>
+    <link rel="stylesheet" href="vendor/font-awesome-4.7.0/css/font-awesome.min.css" type="text/css">
+    <link rel="stylesheet" href="vendor/bootstrap-4.1.1/css/bootstrap.css" type="text/css">
+    <link rel="stylesheet" href="css/light_adminux.css" type="text/css">
+
+
+    <link rel="stylesheet" href="../cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.html" />
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+
+    <style>
+        input {
+            color: #000 !important;
+            font-weight: 600 !important;
+            font-size: 18px !important;
+        }
+    </style>
+    <script src="../../code.tidio.co_443/r9q6huh2ljuktxdjbxh0hap1mc3eihut.js" async></script>
+</head>
+
+<body class="menuclose menuclose-right" style="background:gray">
+    <figure class="background"> <img src="img/login_bg.jpg" alt=" Expressmining "> </figure>
+    <div id="google_translate_element"></div>
+
+    <script type="text/javascript">
+        function googleTranslateElementInit() {
+            new google.translate.TranslateElement({
+                pageLanguage: 'en'
+            }, 'google_translate_element');
+        }
+    </script>
+
+    <script type="text/javascript" src="../translate.google.com/translate_a/elementa0d8a0d8.html?cb=googleTranslateElementInit"></script>
+
+
+
+    <header class="navbar-fixed">
+        <nav class="navbar navbar-toggleable-md sign-in-header">
+            <div class="sidebar-left"> <a class="navbar-brand imglogo" href="login.html"></a> </div>
+            <div class="col"></div>
+            <div class="sidebar-right pull-right">
+                <ul class="navbar-nav  justify-content-end">
+                    <li style="margin:4px"><a href="#" class="btn btn-link text-white"></a></li>
+                    <li style="margin:4px"><a href="../index-2.html" class="btn btn-primary">Home</a></li>
+                    <li style="margin:4px"><a href="login.html" class="btn btn-primary">Login</a></li>
+                    <li style="margin:4px"><a href="register.html" class="btn btn-primary">Register</a></li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+
+<div class="wrapper-content-sign-in p-0">
+    <div class="container text-center">
+        <h1 class="form-signin-heading text-white"><i class="fa fa-"></i>Password Reset</h1>
+        <form class="form-signin1 light" method="POST">
+            <img style="width:100%;height:100%" src="img/logob.png">
+            <span>
+                <h3 style="color:crimson;text-align:center"></h3>
+            </span>
+            <span>
+                <h3 style="color:green;text-align:center"></h3>
+            </span>
+            <h2 class="tex-black mb-4">Reset Password</h2>
+            <label style="color:black">Email address</label>
+            <input type="text" class="form-control" name="email" placeholder="Enter Email address" required>
+            <span style="color:crimson"></span>
+            <br>
+            <input type="submit" name="submit" class="btn btn-lg btn-primary btn-round" style="background:" value="Reset"><br>
+        </form>
+        <p class="mt-3" style="color:crimson">Already have password? <a href="login.html" class="text-white">Login
+                here!</a></p>
+    </div>
+</div>
+
+
+
+
+
+<script src="js/jquery-2.1.1.min.js" type="text/javascript"></script>
+<script src="ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+<script src="vendor/bootstrap4beta/js/bootstrap.min.js" type="text/javascript"></script>
+<script src="vendor/cookie/jquery.cookie.js" type="text/javascript"></script>
+
+
+<script src="js/ie10-viewport-bug-workaround.js"></script>
+<script>
+    "use strict";
+    $('input[type="checkbox"]').on('change', function() {
+        $(this).parent().toggleClass("active")
+        $(this).closest(".media").toggleClass("active");
+    });
+    $(window).on("load", function() {
+        $(".loader_wrapper").fadeOut("slow");
+    });
+</script>
+
+
+</body>
+
+
+<!-- Mirrored from expressmining.com/Dashboard/forgot.php by HTTrack Website Copier/3.x [XR&CO'2014], Sun, 19 Feb 2023 15:14:19 GMT -->
+</html>
